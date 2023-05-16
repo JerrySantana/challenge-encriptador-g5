8e7fa3e9cfaa08f777291574e32889d1eca35976
Gerardo Santana <91979984+JerrySantana@users.noreply.github.com>
2023-05-15 21:08:29 -0600

    añadidos comentarios al codigo del archivo encriptador y al archivo style

--- encriptador.js
+++ encriptador.js
@@ -1,15 +1,15 @@
 /* Created by Gerardo Santana */
-function encryptText() {
-    let originalText = document.getElementById("originalText").value;
-    if(originalText.length === 0) {
+function encryptText() {    // function to encrypt text
+    let originalText = document.getElementById("originalText").value;   // get the text from the input area
+    if(originalText.length === 0) { // check if the input area is empty
         document.getElementById("resultText").placeholder = "Ingresa un texto válido...";
     } else {
-        document.getElementById("resultText").style.backgroundImage = "none";
+        document.getElementById("resultText").style.backgroundImage = "none";   // remove background image from the Decrypted Text area
 
-        let ignorePreviousEncrypt = [];
-        let encryptedText = "";
+        let ignorePreviousEncrypt = []; // array to avoid encrypting the same character twice
+        let encryptedText = ""; // variable to store the final encrypted text
 
-        const encryptions = [
+        const encryptions = [   // array of objects with the character to encrypt and the replacement
             {char: "e", replaceFor: "enter"},
             {char: "o", replaceFor: "ober"},
             {char: "u", replaceFor: "ufat"},
@@ -17,53 +17,53 @@ function encryptText() {
             {char: "a", replaceFor: "ai"},
         ];
 
-        for (let i = 0; i < originalText.length; i++) {
-            let currentChar = originalText.charAt(i);
-            let ignoreCurrentChar = false;
+        for (let i = 0; i < originalText.length; i++) { // loop through the original text
+            let currentChar = originalText.charAt(i);   // get the current character
+            let ignoreCurrentChar = false;  // variable to check if the current character should be ignored
 
-            for (let j = 0; j < ignorePreviousEncrypt.length; j++) {
-                let ignoreEncrypt = ignorePreviousEncrypt[j];
+            for (let j = 0; j < ignorePreviousEncrypt.length; j++) {    // loop through the array of characters to ignore
+                let ignoreEncrypt = ignorePreviousEncrypt[j];   // get the current character to ignore
 
-                if (i >= ignoreEncrypt.start && i < ignoreEncrypt.end) {
-                    encryptedText += currentChar;
-                    ignoreCurrentChar = true;
+                if (i >= ignoreEncrypt.start && i < ignoreEncrypt.end) {    // check if the current character is between the start and end position of the character to ignore
+                    encryptedText += currentChar;   // add the current character to the encrypted text
+                    ignoreCurrentChar = true;   // set the variable to true to ignore the current character
                     break;
                 }
             }
 
-            if (!ignoreCurrentChar) {
-                let foundEncrypt = encryptions.find(
-                    (encrypt) => encrypt.char === currentChar
+            if (!ignoreCurrentChar) {   // if the current character should not be ignored
+                let foundEncrypt = encryptions.find(    // find the character to encrypt
+                    (encrypt) => encrypt.char === currentChar   // check if the current character is the character to encrypt
                 );
 
-                if (foundEncrypt) {
-                    let encryptLength = foundEncrypt.char.length;
-                    let start = i;
-                    let end = i + encryptLength;
+                if (foundEncrypt) { // if the character to encrypt is found
+                    let encryptLength = foundEncrypt.char.length;   // get the length of the character to encrypt
+                    let start = i;  // get the start of the replacement string
+                    let end = i + encryptLength;    // get the end of the replacement string
 
-                    ignorePreviousEncrypt.push({start, end});
-                    encryptedText += foundEncrypt.replaceFor;
-                    i += encryptLength - 1;
+                    ignorePreviousEncrypt.push({start, end});   // add the start and end position of the replacement string to the array of characters to ignore
+                    encryptedText += foundEncrypt.replaceFor;   // add the replacement string to the encrypted text
+                    i += encryptLength - 1; // set the index to the end of the replacement string
                 } else {
-                    encryptedText += currentChar;
+                    encryptedText += currentChar;   // if not adds the current character to the encrypted text
                 }
             }
         }
-        document.getElementById("resultText").value = encryptedText;
-        document.getElementById("copyButton").style.display = "";
+        document.getElementById("resultText").value = encryptedText;    // set the encrypted text to the Decrypted Text area
+        document.getElementById("copyButton").style.display = "";   // show the copy button
     }
 }
 
-function decryptText() {
-    let encryptedText = document.getElementById("originalText").value;
-    if(encryptedText.length === 0) {
+function decryptText() {    // function to decrypt text
+    let encryptedText = document.getElementById("originalText").value;  // get the text from the input area
+    if(encryptedText.length === 0) {    // checks if the input area is empty
         document.getElementById("resultText").placeholder = "Ingresa un texto válido...";
     } else {
-        document.getElementById("resultText").style.backgroundImage = "none";
-        let ignorePreviousDecrypt = [];
-        let decryptedText = "";
+        document.getElementById("resultText").style.backgroundImage = "none";    // remove background image from the Decrypted Text area
+        let ignorePreviousDecrypt = []; // array to avoid decrypting the same character twice
+        let decryptedText = ""; // variable to store the final decrypted text
 
-        const decryptions = [
+        const decryptions = [   // array of objects with the character to decrypt and the replacement
             {encrypt: "enter", replaceFor: "e"},
             {encrypt: "ober", replaceFor: "o"},
             {encrypt: "ufat", replaceFor: "u"},
@@ -71,50 +71,50 @@ function decryptText() {
             {encrypt: "ai", replaceFor: "a"},
         ];
 
-        for (let i = 0; i < encryptedText.length; i++) {
-            let currentChar = encryptedText.charAt(i);
-            let ignoreCurrentChar = false;
+        for (let i = 0; i < encryptedText.length; i++) {    // loop through the encrypted text
+            let currentChar = encryptedText.charAt(i);  // get the current character
+            let ignoreCurrentChar = false;  // variable to check if the current character should be ignored
 
-            for (let j = 0; j < ignorePreviousDecrypt.length; j++) {
-                let ignoreDecrypt = ignorePreviousDecrypt[j];
+            for (let j = 0; j < ignorePreviousDecrypt.length; j++) {    // loop through the array of characters to ignore
+                let ignoreDecrypt = ignorePreviousDecrypt[j];   // get the current character to ignore
 
-                if (i >= ignoreDecrypt.start && i < ignoreDecrypt.end) {
-                    decryptedText += currentChar;
-                    ignoreCurrentChar = true;
+                if (i >= ignoreDecrypt.start && i < ignoreDecrypt.end) {    // check if the current character is between the start and end position of the character to ignore
+                    decryptedText += currentChar;   // add the current character to the decrypted text
+                    ignoreCurrentChar = true;   // set the variable to true to ignore the current character
                     break;
                 }
             }
 
-            if (!ignoreCurrentChar) {
-                let foundDecrypt = decryptions.find(
-                    (decrypt) => decrypt.replaceFor === currentChar
+            if (!ignoreCurrentChar) {    // if the current character should not be ignored
+                let foundDecrypt = decryptions.find(    // find the character to decrypt
+                    (decrypt) => decrypt.replaceFor === currentChar // check if the current character is the character to decrypt
                 );
 
-                if (foundDecrypt) {
-                    let encryptLength = foundDecrypt.encrypt.length;
-                    let start = i;
-                    let end = i + encryptLength;
+                if (foundDecrypt) { // if the character to decrypt is found
+                    let encryptLength = foundDecrypt.encrypt.length;    // get the length of the character to decrypt
+                    let start = i;  // get the start of the replacement string
+                    let end = i + encryptLength;    // get the end of the replacement string
 
-                    ignorePreviousDecrypt.push({start, end});
-                    decryptedText += foundDecrypt.replaceFor;
-                    i += encryptLength - 1;
+                    ignorePreviousDecrypt.push({start, end});   // add the start and end position of the encrypted string to the array of characters to ignore
+                    decryptedText += foundDecrypt.replaceFor;   // add the encrypted string to the decrypted text
+                    i += encryptLength - 1; // set the index to the end of the encrypted string
                 } else {
-                    decryptedText += currentChar;
+                    decryptedText += currentChar;   // it not adds the current character to the decrypted text
                 }
             }
         }
-        document.getElementById("resultText").value = decryptedText;
-        document.getElementById("copyButton").style.display = "";
+        document.getElementById("resultText").value = decryptedText;    // set the decrypted text to the Decrypted Text area
+        document.getElementById("copyButton").style.display = "";   // show the copy button
     }
 }
 
-function copyToClipboard() {
-    document.getElementById("copyButton").style.display = "none";
-    let copiedText = document.getElementById("resultText").value;
-    if (copiedText.length !== 0) {
-        document.getElementById("resultText").value = "";
-        document.getElementById("resultText").placeholder = "Sin texto para mostrar...";
-        document.getElementById("resultText").style.backgroundImage = "";
-        navigator.clipboard.writeText(copiedText);
+function copyToClipboard() {    // function to copy the text in the Decrypted Text area to the clipboard
+    document.getElementById("copyButton").style.display = "none";   // hide the copy button
+    let copiedText = document.getElementById("resultText").value;   // get the text from the Decrypted Text area and store it in a variable
+    if (copiedText.length !== 0) {  // checks if the output text box is not empty
+        document.getElementById("resultText").value = "";   // clear the Decrypted Text area
+        document.getElementById("resultText").placeholder = "Sin texto para mostrar...";    // sets the original placeholder
+        document.getElementById("resultText").style.backgroundImage = "";   // set the background image to the Decrypted Text area
+        navigator.clipboard.writeText(copiedText);  // copy the text of the variable to the clipboard
     }
 }
